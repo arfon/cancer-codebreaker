4bf7b3cda10f60cca936cda07d9c4fc57e159f78
Arfon Smith <arfon@github.com>
2015-06-06 14:44:11 -0500

    Basic questions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :questions do
+    collection do
+      get 'first'
+      get 'second'
+      get 'third'
+    end
+  end
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
